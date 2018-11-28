8468309885930670bedb251628b5d0d595376b03
dstrmv <dstrmv@outlook.com>
2018-11-28 20:22:17 +0400

    lab 9 look&feel implemented

--- src/buildings/GUIMain.java
+++ src/buildings/GUIMain.java
@@ -2,6 +2,8 @@ package buildings;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUIMain {
     public static void main(String[] args) {
@@ -22,10 +24,36 @@ class GUI extends JFrame {
         JMenu menuFile = new JMenu("File");
         JMenuItem dwellingItem = new JMenuItem("Choose dwelling...");
         JMenuItem officeItem = new JMenuItem("Choose office building...");
+        JMenu menuLookFeel = new JMenu("Look&Feel");
+
+        //LOOK AND FEEL
+        ButtonGroup buttonGroup = new ButtonGroup();
+        UIManager.LookAndFeelInfo[] installedLookAndFeels = UIManager.getInstalledLookAndFeels();
+        for (int i = 0; i < installedLookAndFeels.length; i++) {
+            UIManager.LookAndFeelInfo installedLookAndFeel = installedLookAndFeels[i];
+            JRadioButtonMenuItem radioButton = new JRadioButtonMenuItem(installedLookAndFeel.getName());
+            if (installedLookAndFeel.getName().equals(UIManager.getLookAndFeel().getName())) {
+                radioButton.setSelected(true);
+            }
+            radioButton.addActionListener(e -> {
+                try {
+                    UIManager.setLookAndFeel(installedLookAndFeel.getClassName());
+                    SwingUtilities.updateComponentTreeUI(getContentPane());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e1) {
+                    e1.printStackTrace();
+                }
+            });
+            buttonGroup.add(radioButton);
+            menuLookFeel.add(radioButton);
+        }
+
+        dwellingItem.addActionListener(e -> System.out.println("test"));
+
         menuFile.add(dwellingItem);
         menuFile.add(officeItem);
 
         menuBar.add(menuFile);
+        menuBar.add(menuLookFeel);
 
         setJMenuBar(menuBar);
 
